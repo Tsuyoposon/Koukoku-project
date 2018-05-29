71b0c97972551e591580bbef3109957a986edac1
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-05-29 15:56:05 +0900

    [struct]vagrantプロビジョニング

---
+++ provisions/provision_vagrant.sh
@@ -0,0 +1,21 @@
+echo 'start provison_vagrant!!'
+sudo su
+echo $HOME
+
+echo 'pyenv install'
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.profile
+source ~/.profile
+
+echo 'python3.6 install'
+pyenv install 3.6.0
+pyenv global 3.6.0
+python -V
+
+echo 'pip install'
+wget https://bootstrap.pypa.io/get-pip.py
+python3.6 get-pip.py
+pip -V
+pip install pipenv
