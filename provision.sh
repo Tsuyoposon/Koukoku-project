a5cd80d5b20aaa2a63ddaa4e3a111af8ae3ba945
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-05-29 13:24:20 +0900

    [struct]vagrant設定

---
+++ provision.sh
@@ -0,0 +1,19 @@
+sudo su
+apt-get update
+
+apt-get install python3-pip python3-dev
+apt-get install python-pip python-dev
+
+add-apt-repository ppa:jonathonf/python-3.6
+apt-get update
+apt-get install python3.6 python3.6-dev
+wget https://bootstrap.pypa.io/get-pip.py
+python3.6 get-pip.py
+
+pip install pipenv
+
+sudo apt-get install git
+sudo apt-get install vim
+sudo apt-get install gcc-c++
+
+sudo apt-get install -y postgresql
