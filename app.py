b661bfaaf45299b3f2314e76c79e8203248b6b70
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-06-11 16:42:40 +0900

    [modify]flask6

--- app.py
+++ app.py
@@ -1,30 +1,9 @@
-import base64
-import hashlib
-import hmac
-import json
-import os
 from flask import Flask
-from flask import request
-
-
-# Defines a route for the GET request
 app = Flask(__name__)
-app.debug = True
-@app.route('/', methods=['GET'])
-def webhook_challenge():
 
-  # creates HMAC SHA-256 hash from incomming token and your consumer secret
-  sha256_hash_digest = hmac.new("9txEFI1J7FlJI5NfxjKRbtEOk6VE5wP0wXzUvR9KR6EH1m9DtS", msg=request.args.get('crc_token'), digestmod=hashlib.sha256).digest()
-
-  # construct response data with base64 encoded hash
-  response = {
-    'response_token': 'sha256=' + base64.b64encode(sha256_hash_digest)
-  }
-
-  # returns properly formatted json response
-  return json.dumps(response)
-
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+    app.run(host='0.0.0.0')
