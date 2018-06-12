93b74f7ff3c91b12b09b7ab91f98e54ef0e3883e
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-06-12 10:25:00 +0900

    [modify]flaskの修正

--- twitter_receve/receve_api.py
+++ twitter_receve/receve_api.py
@@ -13,11 +13,13 @@ app = Flask(__name__)
 def webhook_challenge():
 
   # creates HMAC SHA-256 hash from incomming token and your consumer secret
-  sha256_hash_digest = hmac.new("9txEFI1J7FlJI5NfxjKRbtEOk6VE5wP0wXzUvR9KR6EH1m9DtS", msg=request.args.get('crc_token'), digestmod=hashlib.sha256).digest()
+  twitter_byte = bytearray("9txEFI1J7FlJI5NfxjKRbtEOk6VE5wP0wXzUvR9KR6EH1m9DtS", "ASCII")
+  crc_token_byte = bytearray(request.args.get('crc_token'), "ASCII")
+  sha256_hash_digest = hmac.new(twitter_byte, msg=crc_token_byte, digestmod=hashlib.sha256).digest()
 
   # construct response data with base64 encoded hash
   response = {
-    'response_token': 'sha256=' + base64.b64encode(sha256_hash_digest)
+    'response_token': 'sha256=' + base64.b64encode(sha256_hash_digest).decode('ASCII')
   }
 
   # returns properly formatted json response
