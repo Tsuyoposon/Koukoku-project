a36fefef793d1ba2a2bf9f01d9ca7644b35e55a6
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-06-15 17:29:27 +0900

    [modify]DMがきた時だけ処理

--- twitter_receve/receve_api.py
+++ twitter_receve/receve_api.py
@@ -13,34 +13,38 @@ app = Flask(__name__)
 @app.route('/webhooks/twitter', methods=['GET'])
 def webhook_challenge():
 
-  # creates HMAC SHA-256 hash from incomming token and your consumer secret
-  twitter_byte = bytearray(os.environ['TWITTER_CONSUMER_SECRET'], "ASCII")
-  crc_token_byte = bytearray(request.args.get('crc_token'), "ASCII")
-  sha256_hash_digest = hmac.new(twitter_byte, crc_token_byte, hashlib.sha256).digest()
+    # creates HMAC SHA-256 hash from incomming token and your consumer secret
+    twitter_byte = bytearray(os.environ['TWITTER_CONSUMER_SECRET'], "ASCII")
+    crc_token_byte = bytearray(request.args.get('crc_token'), "ASCII")
+    sha256_hash_digest = hmac.new(twitter_byte, crc_token_byte, hashlib.sha256).digest()
 
-  # construct response data with base64 encoded hash
-  response = {
+    # construct response data with base64 encoded hash
+    response = {
     'response_token': 'sha256=' + base64.b64encode(sha256_hash_digest).decode('ASCII')
-  }
+    }
 
-  # returns properly formatted json response
-  return json.dumps(response)
+    # returns properly formatted json response
+    return json.dumps(response)
 
 
 # DMを受け取った時の処理
 @app.route('/webhooks/twitter', methods=['POST'])
 def DM_catch():
 
+    # 返信用json
+    respon_json = {"status" : "OK"}
+    print(type(respon_json))
+
+    # webhookされたイベントがDMの送受信の場合だけ処理する
+    if request.json.get("direct_message_events"):
+        print(request.json["direct_message_events"][0]["message_create"]["message_data"]["text"])
+        # 返信を”Get DM”に書き換える
+        respon_json["status"] = "Get DM"
 
 
-  print(request.json)
-  print(request.json["direct_message_events"])
-  print(type(request.json))
-  print(len(request.json))
 
-  # webhookされたイベントがDMの送受信の場合だけ処理する
 
-  return json.dumps({ "status": "OK"})
+    return json.dumps(respon_json)
 
 
 
