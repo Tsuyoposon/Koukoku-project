c9ea4b50a9d6291b431f2786e95bb20759cce4c7
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-06-17 15:17:05 +0900

    [modify]webhookjsonの確認

--- twitter_receve/webhook_process.py
+++ twitter_receve/webhook_process.py
@@ -9,6 +9,7 @@ import json, os
 # DMをもらった時
 def DM_catch(twitter_account, request, respon_json):
 
+    print(json.dumps(request.json, indent=2))
     # オウム返しでDMを返す
     twitter_account.PostDirectMessage(
         request.json["direct_message_events"][0]["message_create"]["message_data"]["text"],
