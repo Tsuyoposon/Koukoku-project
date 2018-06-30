06fd082d0da42f78a40d5571920138cbdac62522
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-06-30 14:51:46 +0900

    [modify]mysqlのプロビジョニング設定

--- provisions/provision_root.sh
+++ provisions/provision_root.sh
@@ -1,4 +1,4 @@
-echo 'start provison_root!!'
+echo '「start provison_root!!」'
 apt-get update
 apt-get install git
 apt-get install vim
@@ -6,4 +6,8 @@ apt-get install vim
 apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libbz2-dev libreadline-dev
 apt-get install -y postgresql
 
-apt-get install mysql-server
+apt-get -y install expect
+
+echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+apt-get -y install mysql-server
