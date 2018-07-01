70d7cb71dc08be584774b0da68532c21b3237370
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-07-01 21:45:36 +0900

    [CI]werckerでmysql8

--- twitter_receve/koukokuDB/config.py
+++ twitter_receve/koukokuDB/config.py
@@ -8,6 +8,12 @@ class DevelopmentConfig:
         'password': os.getenv('DB_PASSWORD', 'root'),
         'host': os.getenv('DB_HOST', 'localhost'),
     })
+    if os.environ['ENV'] == "wercker":
+            SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/koukokuDB?charset=utf8'.format(**{
+                'user': os.getenv('DB_USER', 'root'),
+                'password': os.getenv('DB_PASSWORD', 'root'),
+                'host': os.environ['MYSQL_PORT_3306_TCP_ADDR']:os.environ['MYSQL_PORT_3306_TCP_PORT'],
+            })
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
