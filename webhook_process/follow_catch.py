360782cc2bb7e8650da7c4eb8178bb9f300acbce
Tsuyoposon <sinngo_band@yahoo.co.jp>
2018-11-19 16:24:40 +0900

    [modify]関数に分割する

--- webhook_process/follow_catch.py
+++ webhook_process/follow_catch.py
@@ -19,28 +19,8 @@ from DB.koukokuDB.database import db
 def process(twitter_account_auth, watson_personal_API, request, respon_json):
 
     if request.json["follow_events"][0]["source"]["id"] != os.environ['MYTWITTER_ACCOUNT_ID']:
-
         # 自分が相手をフォローしているか確認する
-        friendships_result = requests.get(
-            "https://api.twitter.com/1.1/friendships/lookup.json",
-            auth=twitter_account_auth,
-            params={
-                "user_id" : request.json["follow_events"][0]["source"]["id"],
-            }
-        )
-        if "following" not in friendships_result.json()[0]["connections"]:
-            # もし相手をフォローしていなかったらフォロー返しをする
-            requests.post(
-                "https://api.twitter.com/1.1/friendships/create.json",
-                auth=twitter_account_auth,
-                params={
-                    "user_id" : request.json["follow_events"][0]["source"]["id"],
-                    "follow"  : "true"
-                }
-            )
-            respon_json["Follow"] = "OK"
-        else:
-            respon_json["Follow"] = "NO"
+        respon_json["Follow"] = check_friendship(request.json["follow_events"][0]["source"]["id"], twitter_account_auth)
 
         # DB内に登録しようとしているユーザが既にいるのか確認する
         check_user = User.query.filter_by(twitter_userid=request.json["follow_events"][0]["source"]["id"]).first()
@@ -54,13 +34,11 @@ def process(twitter_account_auth, watson_personal_API, request, respon_json):
                     "count"   : 10
                     }
                 )
-
             # timelineの文章
             DM_user_linked_timeline = ""
             for DM_user_tweet in DM_user_timeline.json():
                 DM_user_linked_timeline += DM_user_tweet["text"]
-
-            # watsonにテキストを送る
+            # watsonにツイート10件を送る
             watson_renponse = watson_personal_API.profile(
                 DM_user_linked_timeline,
                 content_type="text/plain",
@@ -68,63 +46,8 @@ def process(twitter_account_auth, watson_personal_API, request, respon_json):
                 content_language="ja"
             )
             # フォローしたユーザの性格情報をいれる
-            user = User(
-                request.json["follow_events"][0]["source"]["id"],
-                watson_renponse["personality"][0]["percentile"],
-                watson_renponse["personality"][0]["children"][0]["percentile"],
-                watson_renponse["personality"][0]["children"][1]["percentile"],
-                watson_renponse["personality"][0]["children"][2]["percentile"],
-                watson_renponse["personality"][0]["children"][3]["percentile"],
-                watson_renponse["personality"][0]["children"][4]["percentile"],
-                watson_renponse["personality"][0]["children"][5]["percentile"],
-                watson_renponse["personality"][1]["percentile"],
-                watson_renponse["personality"][1]["children"][0]["percentile"],
-                watson_renponse["personality"][1]["children"][1]["percentile"],
-                watson_renponse["personality"][1]["children"][2]["percentile"],
-                watson_renponse["personality"][1]["children"][3]["percentile"],
-                watson_renponse["personality"][1]["children"][4]["percentile"],
-                watson_renponse["personality"][1]["children"][5]["percentile"],
-                watson_renponse["personality"][2]["percentile"],
-                watson_renponse["personality"][2]["children"][0]["percentile"],
-                watson_renponse["personality"][2]["children"][1]["percentile"],
-                watson_renponse["personality"][2]["children"][2]["percentile"],
-                watson_renponse["personality"][2]["children"][3]["percentile"],
-                watson_renponse["personality"][2]["children"][4]["percentile"],
-                watson_renponse["personality"][2]["children"][5]["percentile"],
-                watson_renponse["personality"][3]["percentile"],
-                watson_renponse["personality"][3]["children"][0]["percentile"],
-                watson_renponse["personality"][3]["children"][1]["percentile"],
-                watson_renponse["personality"][3]["children"][2]["percentile"],
-                watson_renponse["personality"][3]["children"][3]["percentile"],
-                watson_renponse["personality"][3]["children"][4]["percentile"],
-                watson_renponse["personality"][3]["children"][5]["percentile"],
-                watson_renponse["personality"][4]["percentile"],
-                watson_renponse["personality"][4]["children"][0]["percentile"],
-                watson_renponse["personality"][4]["children"][1]["percentile"],
-                watson_renponse["personality"][4]["children"][2]["percentile"],
-                watson_renponse["personality"][4]["children"][3]["percentile"],
-                watson_renponse["personality"][4]["children"][4]["percentile"],
-                watson_renponse["personality"][4]["children"][5]["percentile"],
-                watson_renponse["needs"][0]["percentile"],
-                watson_renponse["needs"][1]["percentile"],
-                watson_renponse["needs"][2]["percentile"],
-                watson_renponse["needs"][3]["percentile"],
-                watson_renponse["needs"][4]["percentile"],
-                watson_renponse["needs"][5]["percentile"],
-                watson_renponse["needs"][6]["percentile"],
-                watson_renponse["needs"][7]["percentile"],
-                watson_renponse["needs"][8]["percentile"],
-                watson_renponse["needs"][9]["percentile"],
-                watson_renponse["needs"][10]["percentile"],
-                watson_renponse["needs"][11]["percentile"],
-                watson_renponse["values"][0]["percentile"],
-                watson_renponse["values"][1]["percentile"],
-                watson_renponse["values"][2]["percentile"],
-                watson_renponse["values"][3]["percentile"],
-                watson_renponse["values"][4]["percentile"]
-            )
-            db.session.add(user)
-            db.session.commit()
+            insert_user(watson_renponse, request.json["follow_events"][0]["source"]["id"])
+
             # ”ユーザ情報をDBに書き込んだ”という返信
             respon_json["New User"] = "OK"
             return json.dumps(respon_json)
@@ -137,3 +60,85 @@ def process(twitter_account_auth, watson_personal_API, request, respon_json):
         respon_json["New User"] = "NO"
         respon_json["Follow"] = "NO"
         return json.dumps(respon_json)
+
+def check_friendship(twitter_ID, twitter_account_auth):
+    # 自分が相手をフォローしているか確認する
+    friendships_result = requests.get(
+        "https://api.twitter.com/1.1/friendships/lookup.json",
+        auth=twitter_account_auth,
+        params={
+            "user_id" : twitter_ID,
+        }
+    )
+    if "following" not in friendships_result.json()[0]["connections"]:
+        # もし相手をフォローしていなかったらフォロー返しをする
+        requests.post(
+            "https://api.twitter.com/1.1/friendships/create.json",
+            auth=twitter_account_auth,
+            params={
+                "user_id" : twitter_ID,
+                "follow"  : "true"
+            }
+        )
+        return "OK"
+    else:
+        return "NO"
+
+def insert_user(watson_renponse, twitter_ID):
+    user = User(
+        twitter_ID,
+        watson_renponse["personality"][0]["percentile"],
+        watson_renponse["personality"][0]["children"][0]["percentile"],
+        watson_renponse["personality"][0]["children"][1]["percentile"],
+        watson_renponse["personality"][0]["children"][2]["percentile"],
+        watson_renponse["personality"][0]["children"][3]["percentile"],
+        watson_renponse["personality"][0]["children"][4]["percentile"],
+        watson_renponse["personality"][0]["children"][5]["percentile"],
+        watson_renponse["personality"][1]["percentile"],
+        watson_renponse["personality"][1]["children"][0]["percentile"],
+        watson_renponse["personality"][1]["children"][1]["percentile"],
+        watson_renponse["personality"][1]["children"][2]["percentile"],
+        watson_renponse["personality"][1]["children"][3]["percentile"],
+        watson_renponse["personality"][1]["children"][4]["percentile"],
+        watson_renponse["personality"][1]["children"][5]["percentile"],
+        watson_renponse["personality"][2]["percentile"],
+        watson_renponse["personality"][2]["children"][0]["percentile"],
+        watson_renponse["personality"][2]["children"][1]["percentile"],
+        watson_renponse["personality"][2]["children"][2]["percentile"],
+        watson_renponse["personality"][2]["children"][3]["percentile"],
+        watson_renponse["personality"][2]["children"][4]["percentile"],
+        watson_renponse["personality"][2]["children"][5]["percentile"],
+        watson_renponse["personality"][3]["percentile"],
+        watson_renponse["personality"][3]["children"][0]["percentile"],
+        watson_renponse["personality"][3]["children"][1]["percentile"],
+        watson_renponse["personality"][3]["children"][2]["percentile"],
+        watson_renponse["personality"][3]["children"][3]["percentile"],
+        watson_renponse["personality"][3]["children"][4]["percentile"],
+        watson_renponse["personality"][3]["children"][5]["percentile"],
+        watson_renponse["personality"][4]["percentile"],
+        watson_renponse["personality"][4]["children"][0]["percentile"],
+        watson_renponse["personality"][4]["children"][1]["percentile"],
+        watson_renponse["personality"][4]["children"][2]["percentile"],
+        watson_renponse["personality"][4]["children"][3]["percentile"],
+        watson_renponse["personality"][4]["children"][4]["percentile"],
+        watson_renponse["personality"][4]["children"][5]["percentile"],
+        watson_renponse["needs"][0]["percentile"],
+        watson_renponse["needs"][1]["percentile"],
+        watson_renponse["needs"][2]["percentile"],
+        watson_renponse["needs"][3]["percentile"],
+        watson_renponse["needs"][4]["percentile"],
+        watson_renponse["needs"][5]["percentile"],
+        watson_renponse["needs"][6]["percentile"],
+        watson_renponse["needs"][7]["percentile"],
+        watson_renponse["needs"][8]["percentile"],
+        watson_renponse["needs"][9]["percentile"],
+        watson_renponse["needs"][10]["percentile"],
+        watson_renponse["needs"][11]["percentile"],
+        watson_renponse["values"][0]["percentile"],
+        watson_renponse["values"][1]["percentile"],
+        watson_renponse["values"][2]["percentile"],
+        watson_renponse["values"][3]["percentile"],
+        watson_renponse["values"][4]["percentile"]
+    )
+    db.session.add(user)
+    db.session.commit()
