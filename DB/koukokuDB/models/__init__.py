fcd55f5c49a0809c946f6d9b96f61b3af5361136
vagrant <vagrant@vagrant.vm>
2018-11-21 07:13:44 +0000

    [add]推薦アイテムテーブル

--- DB/koukokuDB/models/__init__.py
+++ DB/koukokuDB/models/__init__.py
@@ -1,5 +1,7 @@
 from .users import User
+from .recommen_items import Recommen_item
 
 __all__ = [
     User,
+    Recommen_item,
 ]
---
+++ DB/koukokuDB/models/recommen_items.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from DB.koukokuDB.database import db
+
+class Recommen_item(db.Model):
+
+    __tablename__ = 'recommen_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    recommen_item_name = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    def __init__(self, recommen_item_name):
+        self.recommen_item_name = recommen_item_name
+
+    def __repr__(self):
+        return '<recommen_item_name %r>' % self.recommen_item_name
