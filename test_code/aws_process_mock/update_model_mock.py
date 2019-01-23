502f09cb14a1e4f97ececebddf2ab7fd3caf57a9
Tsuyoposon <sinngo_band@yahoo.co.jp>
2019-01-23 19:41:03 +0900

    [test]その他DMを送った時の警告メッセージ

--- test_code/aws_process_mock/update_model_mock.py
+++ test_code/aws_process_mock/update_model_mock.py
@@ -18,3 +18,19 @@ def boto3_resource(*args, **kwargs):
     if args[0] == "s3":
         return MockBucket
     return None
+
+# twitterAPIの動作を再現
+def mocked_twitter_API(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+        def json(self):
+            return self.json_data
+    # DMを送る時のrequest(POST)
+    if args[0] == "https://api.twitter.com/1.1/direct_messages/events/new.json":
+        catch_json = json.loads(kwargs["data"])
+        # 警告メッセージが届いた時
+        if catch_json["event"]["message_create"]["target"]["recipient_id"] == os.environ['TEST_ACCOUNT_ID'] and \
+        catch_json["event"]["message_create"]["message_data"]["text"] is not None:
+            return MockResponse({}, 200)
